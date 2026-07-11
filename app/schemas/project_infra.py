ea808805a0f96b5cd298a02bf54983a0f9674f58
Arnab Senapati <senapatiarnab676@gmail.com>
2026-07-11 14:28:27 +0530

    feat: add email fields to project infrastructure schema

--- app/schemas/project_infra.py
+++ app/schemas/project_infra.py
@@ -14,28 +14,40 @@ class UserBase(BaseModel):
 
 class ProjectInfraCreate(BaseModel):
     project_name: str
-    email_used: str
+    email_used: Optional[str] = ""
     frontend_url: Optional[str] = ""
+    frontend_email: Optional[str] = ""
     backend_url: Optional[str] = ""
+    backend_email: Optional[str] = ""
     database_url: Optional[str] = ""
+    database_email: Optional[str] = ""
     server_location: Optional[str] = ""
+    server_email: Optional[str] = ""
 
 class ProjectInfraUpdate(BaseModel):
     project_name: Optional[str] = None
     email_used: Optional[str] = None
     frontend_url: Optional[str] = None
+    frontend_email: Optional[str] = None
     backend_url: Optional[str] = None
+    backend_email: Optional[str] = None
     database_url: Optional[str] = None
+    database_email: Optional[str] = None
     server_location: Optional[str] = None
+    server_email: Optional[str] = None
 
 class ProjectInfraInDB(BaseModel):
     id: PyObjectId = Field(alias="_id")
     project_name: str
-    email_used: str
+    email_used: str = ""
     frontend_url: str = ""
+    frontend_email: str = ""
     backend_url: str = ""
+    backend_email: str = ""
     database_url: str = ""
+    database_email: str = ""
     server_location: str = ""
+    server_email: str = ""
     created_by: UserBase
     created_at: datetime
     updated_at: datetime
